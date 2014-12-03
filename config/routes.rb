8f15d0ecaad6145fbd1673ac7b77406cd5a1c6c5
Lukas Appelhans <l.appelhans@gmx.de>
2014-12-03 13:15:36 +0100

    Add skeleton for promotions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :absence_periods
+  resources :promotions, only: [:show, :index, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
