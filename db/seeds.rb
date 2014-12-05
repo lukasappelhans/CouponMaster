cb47786246e42518ba20ea4b3999128398f138a1
Lukas Appelhans <l.appelhans@gmx.de>
2014-12-05 13:41:49 +0100

    Add admin column to users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(id: 1, email: "test@test.org", password: "password", password_confirmation: "password")
+User.create!(id: 1, email: "test@test.org", password: "password", password_confirmation: "password", admin: false)
+User.create!(id: 2, email: "admin@test.org", password: "password", password_confirmation: "password", admin: true)
 AbsencePeriod.create!(user_id: 1, startdate: 5.minutes.ago, enddate: 5.minutes.ago)
 AbsencePeriod.create!(user_id: 1, startdate: 3.minutes.ago, enddate: 2.minutes.ago)
