6e9a2bfb0882a76a4b3e49fcd3be994249ffba73
Lukas Appelhans <l.appelhans@gmx.de>
2014-12-12 01:03:03 +0100

    Better date formatting and add new seeds to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,15 @@
 
 User.create!(id: 1, email: "test@test.org", password: "password", password_confirmation: "password", admin: false)
 User.create!(id: 2, email: "admin@test.org", password: "password", password_confirmation: "password", admin: true)
-AbsencePeriod.create!(user_id: 1, startdate: 5.minutes.ago, enddate: 5.minutes.ago)
-AbsencePeriod.create!(user_id: 1, startdate: 3.minutes.ago, enddate: 2.minutes.ago)
+AbsencePeriod.create!(user_id: 1, startdate: 5.minutes.ago, enddate: 10.days.from_now)
+AbsencePeriod.create!(user_id: 1, startdate: 15.days.from_now, enddate: 25.days.from_now)
 
-Product.create!(id: 1, name: "Product one", description: "This is product one!", price: 3)
-Product.create!(id: 2, name: "Premium product", description: "Whatever", price: 100)
+Product.create!(id: 1, name: "El periodico", description: "Subscribe to el periodico!", price: 5)
+Product.create!(id: 2, name: "Promoted magazine", description: "This is a promoted product!", price: 10)
+Product.create!(id: 3, name: "La vanguardia", description: "This is a newspaper in spanish!", price: 3)
+Product.create!(id: 4, name: "ara.cat", description: "Catalan newspaper!", price: 5)
 
 Newspaper.create!(product_id: 1)
+Newspaper.create!(product_id: 3)
+Newspaper.create!(product_id: 4)
 Promotion.create!(product_id: 2, startdate: 5.minutes.from_now, enddate: 10.days.from_now)
